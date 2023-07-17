9566001b971640601b026e040dd3acef6e3f2697
vkray <vitalikkrainyuk@yandex.ru>
2023-07-17 22:01:04 +0300

    hw2. Коллекции: многообразие реализаций

--- src/main/java/com/example/EmployeeBook/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/EmployeeBook/service/EmployeeServiceImpl.java
@@ -5,35 +5,32 @@ import com.example.EmployeeBook.exception.EmployeeNotFoundException;
 import com.example.EmployeeBook.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final List<Employee> employeeList;
+    private final Map<String, Employee> employees;
 
     public EmployeeServiceImpl() {
-        this.employeeList = new ArrayList<>();
+        this.employees = new HashMap<>();
     }
 
     @Override
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if(employeeList.contains(employee)){
+        if(employees.containsKey(employee.getFullName())){
             throw new EmployeeAlreadyAddedException();
         }
-        employeeList.add(employee);
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if(employeeList.contains(employee)) {
-            employeeList.remove(employee);
-            return employee;
+        if(employees.containsKey(employee.getFullName())) {
+            return employees.remove(employee.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
@@ -41,15 +38,15 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if(employeeList.contains(employee)) {
-            return employee;
+        if(employees.containsKey(employee.getFullName())) {
+            return employees.get(employee.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
 
     @Override
     public Collection<Employee> findAll() {
-        return Collections.unmodifiableList(employeeList);
+        return Collections.unmodifiableCollection(employees.values());
     }
 
 }
